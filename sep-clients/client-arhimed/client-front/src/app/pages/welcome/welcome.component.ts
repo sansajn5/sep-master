9261ecf9f2ca54f99b2e89079c73f599f763c35b
kovacevicmilica544 <kovacevicmilica544@gmail.com>
2020-01-31 23:22:37 +0100

    working on scientific center

--- sep-clients/client-arhimed/client-front/src/app/pages/welcome/welcome.component.ts
+++ sep-clients/client-arhimed/client-front/src/app/pages/welcome/welcome.component.ts
@@ -16,8 +16,15 @@ export class WelcomeComponent implements OnInit {
 
   private userIsValid: boolean = false;
   private payments = [];
-  public amount: string = '50';
+  public amount: string = '50.00';
   public clientId = "92aa3e6b-2594-11ea-b565-0242ac150005"
+
+  public period = 12;
+  public frequency = 'MONTH';
+  public productId = '123456789';
+  public magazine = 'Magazine Today Physic';
+  public referenceId = '123';
+  public merchantId = '17ec969e-b845-41f7-824a-2a1877406e78';
 
   constructor(
     private route: ActivatedRoute,
@@ -36,23 +43,40 @@ export class WelcomeComponent implements OnInit {
   public getPaymentMethods() {
     this.dummyService.getPaymentMethods().subscribe(data => {
       this.payments = data.payments;
+    }, err => {
+      alert(JSON.stringify(err));
     })
   }
 
   public goTo(payment) {
-    if (payment.name == 'BANK') {
-      window.open(payment.paymentUrl + "/" + merchantId + "/" + this.clientId + "/" + this.amount, "_blank");
+    const body = {
+      productId: this.productId,
+      price: this.amount
     }
-    if (payment.name == 'PAY_PAL') {
-      // TODO use paymentUrl
-      // window.open(payment.paymentUrl)
-      window.open('http://localhost:4100/payment?customerId=1&sellerId=' + this.clientId +'&vendorId=' + merchantId + '&productId=1&price=' + this.amount)
-    }
-    if (payment.name == 'BITCOIN') {
-      http://localhost:4200/payment?organizationId=51&customerId=3&merchantId=92aa3e6b-2594-11ea-b565-0242ac150005&productId=5&price_amount=0.003&price_currency=BTC&receive_currency=BTC
-      window.open('http://localhost:4300/payment?organizationId=' + merchantId +'&customerId='+ this.clientId + '&merchantId='+ merchantId +'&productId=5&price_amount=0.003&price_currency=BTC&receive_currency=BTC')
-    }
-    console.log(payment.paymentUrl);
+    this.dummyService.createTransaction(body).subscribe(data => {
+      this.referenceId = data._id;
+      console.log(this.referenceId);
+      
+      if (payment.name == 'BANK') {
+        window.open(payment.paymentUrl + "/" + merchantId + "/" + this.clientId + "/" + this.amount, "_blank");
+      }
+      if (payment.name == 'PAYPAL') {
+        // TODO use paymentUrl
+         window.open(payment.paymentUrl + `?userId=${this.userIsValid}&merchantId=${this.merchantId}&productId=${this.productId}&price=${this.amount}&referenceId=${this.referenceId}`);
+       // window.open('http://localhost:4100/payment?customerId=1&sellerId=' + this.clientId +'&vendorId=' + merchantId + '&productId=1&price=' + this.amount)
+      }
+      if (payment.name == 'BITCOIN') {
+        http://localhost:4200/payment?organizationId=51&customerId=3&merchantId=92aa3e6b-2594-11ea-b565-0242ac150005&productId=5&price_amount=0.003&price_currency=BTC&receive_currency=BTC
+        window.open('http://localhost:4300/payment?organizationId=' + merchantId +'&customerId='+ this.clientId + '&merchantId='+ merchantId +'&productId=5&price_amount=0.003&price_currency=BTC&receive_currency=BTC')
+      }
+      if (payment.name == 'SUBSCRIBE') {
+        window.open(payment.paymentUrl + `?magazine=${this.magazine}&amount=${this.amount}&period=${this.period}&productId=${this.productId}&frequency=${this.frequency}&userId=${this.userIsValid}&merchantId=${this.merchantId}`);
+      }
+      console.log(payment.paymentUrl);
+    }, err=> {
+      alert(JSON.stringify(err));
+    })
+    
   }
 
 }
--- sep-clients/client-arhimed/client-front/src/app/services/dummy-service.service.ts
+++ sep-clients/client-arhimed/client-front/src/app/services/dummy-service.service.ts
@@ -11,8 +11,13 @@ export class DummyServiceService {
 
   constructor(private http: HttpClient) {}
 
-
   public getPaymentMethods(): Observable<any> {
-    return this.http.get('http://localhost:8010/payments');
+   // return this.http.get('http://localhost:8010/payments');
+   return this.http.get('http://localhost:8080/payments');
   }
+
+  public createTransaction(data: any): Observable<any> {
+    return this.http.post('http://localhost:8080/transaction', data);
+   }
+
 }
